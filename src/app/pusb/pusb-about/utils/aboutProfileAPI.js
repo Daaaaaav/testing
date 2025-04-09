235d9767540a397d6c6b0b04bc071d619e902717
Daaaaaav <scarletstormsubs@gmail.com>
2025-04-09 17:31:38 +0700

    proxy update2

--- src/app/pusb/pusb-about/utils/aboutProfileAPI.js
+++ src/app/pusb/pusb-about/utils/aboutProfileAPI.js
@@ -3,15 +3,22 @@ export async function GetPUSBProfile() {
     const proxyUrl = `/api/proxy?url=${encodeURIComponent("https://api.pusb.or.id/v1/profile")}`;
     const res = await fetch(proxyUrl);
 
-    console.log("Proxy response status:", res.status);
     const text = await res.text();
+    console.log("Proxy response status:", res.status);
     console.log("Raw proxy response:", text);
 
-    const data = JSON.parse(text);
-    return data;
+    if (!text || text.trim() === "") throw new Error("Profile data is empty");
+    
+    const contentType = res.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return JSON.parse(text);
+    } else {
+      throw new Error("Unexpected response format");
+    }
   } catch (err) {
     console.error("Error fetching PUSB Profile:", err);
     return null;
   }
 }
+
 
